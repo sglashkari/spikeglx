6bf1e549e806c348463e1e35b31a2724776cc70a
Shahin <gharakozlou@gmail.com>
2022-04-29 16:32:19 -0400

    Fixing a bug in the code for volt per bit calculations by reading the gains from the meta files

--- apbin2ncs.m
+++ apbin2ncs.m
@@ -10,7 +10,7 @@
 %
 %   See also APBIN2NCS, WINCLUST2MAT.
 %
-% Date 2022-01-05
+% Date 2022-04-29
 % Author Shahin G Lashkari
 %
 % Check:
@@ -42,21 +42,44 @@ if isa(selparentpath,'double')
     return;
 end
 
-answer=questdlg('Which Neuropixels probe have you used?', 'Warning!', 'NP 1.0', 'NP 2.0', 'NP 2.0');
-if isequal(answer,'NP 1.0')
+chunksize = 300; % 300 samples of length 512, almost 5 seconds: 5 sec x 30Khz / 512 sample
+
+% updated 4/29 https://billkarsh.github.io/SpikeGLX/Sgl_help/Metadata_30.html
+meta = ReadMeta(binaryFile{1}, path);
+nChan = str2double(meta.nSavedChans); % 385
+if meta.imDatPrb_type == "0"
     disp('Neuropixels 1.0 is selected!')
     NP = 1;
     peak2peak = 5*1e-3; % 5 mV for NP 1.0
     bits = 10; % 10-bit for NP 1.0
-else
+    % For type 0 imec probes:
+    Imax = str2double(meta.imMaxInt);
+    Vmax = str2double(meta.imAiRangeMax);
+    % reading AP gains from IMRO table in the meta file
+    C = textscan(meta.imroTbl, '%s', 'Delimiter', ')(' );
+    C = {C{1}{4:2:end}};
+    gain = zeros(1,nChan);
+    for i=1:nChan-1
+        imroArray = cell2mat(textscan(C{i}, '%d %d %d %d %d', 'Delimiter', ' '));
+        gain(i) = imroArray(4);
+    end
+elseif meta.imDatPrb_type == "21" || meta.imDatPrb_type == "24"
     disp('Neuropixels 2.0 is selected!')
     NP = 2;
     peak2peak = 12.5*1e-3; % 12.5 mV for NP 2.0
     bits = 14; % 14-bit for NP 2.0
+    % For type 21 or type 24 imec probes:
+    Imax = str2double(meta.imMaxInt);
+    Vmax = str2double(meta.imAiRangeMax);
+    gain = 80*ones(1,nChan);
+else
+    error(['Type ' meta.imDatPrb_type ' is not supported!'])
 end
-chunksize = 300; % 300 samples of length 512, almost 5 seconds: 5 sec x 30Khz / 512 sample
+% voltperbit = peak2peak/2^bits
+voltperbit = Vmax ./ Imax ./ gain;
 Nlx_ADBitVolts = 0.000000036621093749999997;
-voltperbit = peak2peak/2^bits/Nlx_ADBitVolts;
+Nlx_bits_per_NP_bits = voltperbit/Nlx_ADBitVolts;
+
 
 Vyashpath = 'C:\Users\neuropixels\Neuropixels\NPtoWinclust_Pipeline\makeParms';
 Vyashpath = uigetdir(Vyashpath,'Select Vyash''s Code Directory');
@@ -119,7 +142,7 @@ for l = 1:length(csvFile)
                 Samples = fread(binFile, [nChan, 512*chunksize], 'int16=>double')'; % (512xchunksize) x 385
                 Samples(:,range) = Samples(:,range) - mean(Samples(:,range),1); % remove DC offset
                 Samples(:,range) = Samples(:,range) - median(Samples(:,range),2); % CAR (changed from mean to median SGL 2021-01-21)
-                Samples = -voltperbit * Samples; % microvolts to bits INVERTED
+                Samples(:,range) = - Samples(:,range); % INVERTED
                 
                 TimeStamp = (tStart{k} - tStart{1} + (i-1)*512*chunksize*dt:512*dt:(i*512*chunksize-1)*dt )*1e6; % in microseconds
                 Ttime = (i-1)*512*chunksize*dt:dt:(i*512*chunksize-1)*dt;
@@ -136,7 +159,7 @@ for l = 1:length(csvFile)
                         fwrite(cscFile{j}, SampleFreq, 'uint32');
                         fwrite(cscFile{j}, NumValidSamples, 'uint32');
                         % 512 x N
-                        fwrite(cscFile{j}, Samples((l-1)*512+1:l*512,j), 'int16');
+                        fwrite(cscFile{j}, Nlx_bits_per_NP_bits(j) * Samples((l-1)*512+1:l*512,j), 'int16'); % Convert NP bits to NLX bits
                     end
                 end
                 if mod(i,5*round(n/100))==0 % display progress every 5 percent
